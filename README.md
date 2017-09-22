5ef4ae32467cc24c4a1fa0a34d1eb4d0b6f73883
Alexis <flexibeast@gmail.com>
2017-09-22 12:52:21 +1000

    Rename package from `pulsectl` to `pulseaudio-control`.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# pulsectl - Use `pactl` to manage PulseAudio volumes
+# pulseaudio-control - Use `pactl` to manage PulseAudio volumes
 
 *Author:* Alexis <flexibeast@gmail.com><br>
-*URL:* [https://github.com/flexibeast/pulsectl](https://github.com/flexibeast/pulsectl)<br>
+*Version:* 0.1<br>
 
-`pulsectl` controls PulseAudio volumes from Emacs, via `pactl`.
+`pulseaudio-control` controls PulseAudio volumes from Emacs, via `pactl`.
 
 ## Table of Contents
 
@@ -14,47 +14,47 @@
 
 ## Installation
 
-Install [pulsectl from MELPA](http://melpa.org/#/pulsectl), or put `pulsectl.el` in your load-path and do a `(require 'pulsectl)`.
+Install [pulseaudio-control from MELPA](http://melpa.org/#/pulseaudio-control), or put `pulseaudio-control.el` in your load-path and do a `(require 'pulseaudio-control)`.
 
 ## Usage
 
-Use <kbd>C-x /</kbd> to access the `pulsectl` keymap. The default keybindings in that keymap are:
+Use <kbd>C-x /</kbd> to access the `pulseaudio-control` keymap.  The default keybindings in that keymap are:
 
-* <kbd>&#0043;</kbd> : Increase the volume of the currently-selected sink by `pulsectl-volume-step` (`pulsectl-increase-volume`).
+* <kbd>&#0043;</kbd> : Increase the volume of the currently-selected sink by `pulseaudio-control-volume-step` (`pulseaudio-control-increase-volume`).
 
-* <kbd>&#0045;</kbd> : Decrease the volume of the currently-selected sink by `pulsectl-volume-step` (`pulsectl-decrease-volume`).
+* <kbd>&#0045;</kbd> : Decrease the volume of the currently-selected sink by `pulseaudio-control-volume-step` (`pulseaudio-control-decrease-volume`).
 
-* <kbd>v</kbd> : Directly specify the volume of the currently-selected sink (`pulsectl-set-volume`). The value can be:
+* <kbd>v</kbd> : Directly specify the volume of the currently-selected sink (`pulseaudio-control-set-volume`).  The value can be:
 
   * a percentage, e.g. '10%';
   * in decibels, e.g. '2dB';
   * a linear factor, e.g. '0.9' or '1.1'.
 
-* <kbd>m</kbd> : Toggle muting of the currently-selected sink (`pulsectl-toggle-current-sink-mute`).
+* <kbd>m</kbd> : Toggle muting of the currently-selected sink (`pulseaudio-control-toggle-current-sink-mute`).
 
-* <kbd>x</kbd> : Toggle muting of a sink, specified by index (`pulsectl-toggle-sink-mute-by-index`).
+* <kbd>x</kbd> : Toggle muting of a sink, specified by index (`pulseaudio-control-toggle-sink-mute-by-index`).
 
-* <kbd>e</kbd> : Toggle muting of a sink, specified by name (`pulsectl-toggle-sink-mute-by-name`).
+* <kbd>e</kbd> : Toggle muting of a sink, specified by name (`pulseaudio-control-toggle-sink-mute-by-name`).
 
-* <kbd>i</kbd> : Select a sink to be the current sink, specified by index (`pulsectl-select-sink-by-index`).
+* <kbd>i</kbd> : Select a sink to be the current sink, specified by index (`pulseaudio-control-select-sink-by-index`).
 
-* <kbd>n</kbd> : Select a sink to be the current sink, specified by name (`pulsectl-select-sink-by-name`).
+* <kbd>n</kbd> : Select a sink to be the current sink, specified by name (`pulseaudio-control-select-sink-by-name`).
 
-Customisation options, including `pulsectl-volume-step`, are available via the `pulsectl` customize-group.
+Customisation options, including `pulseaudio-control-volume-step`, are available via the `pulseaudio-control` customize-group.
 
 ## Issues / bugs
 
-If you discover an issue or bug in `pulsectl` not already noted:
+If you discover an issue or bug in `pulseaudio-control` not already noted:
 
 * as a TODO item, or
 
-* in [the project's "Issues" section on GitHub](https://github.com/flexibeast/pulsectl/issues),
+* in [the project's "Issues" section on GitHub](https://github.com/flexibeast/pulseaudio-control/issues),
 
 please create a new issue with as much detail as possible, including:
 
 * which version of Emacs you're running on which operating system, and
 
-* how you installed `pulsectl`.
+* how you installed `pulseaudio-control`.
 
 ## License
 
--- pulsectl.el
+++ pulsectl.el
@@ -1,11 +1,11 @@
-;;; pulsectl.el --- Use `pactl' to manage PulseAudio volumes.
+;;; pulseaudio-control.el --- Use `pactl' to manage PulseAudio volumes.
 
 ;; Copyright (C) 2017  Alexis <flexibeast@gmail.com>
 
 ;; Author: Alexis <flexibeast@gmail.com>
 ;; Maintainer: Alexis <flexibeast@gmail.com>
 ;; Created: 2017-08-23
-;; URL: https://github.com/flexibeast/pulsectl
+;; URL: https://github.com/flexibeast/pulseaudio-control
 ;; Keywords: multimedia, hardware, sound, PulseAudio
 ;; Version: 0.1
 ;; Package-Version: 0.1
@@ -30,7 +30,7 @@
 
 ;;; Commentary:
 
-;; `pulsectl' controls PulseAudio volumes from Emacs, via `pactl`.
+;; `pulseaudio-control' controls PulseAudio volumes from Emacs, via `pactl`.
 
 ;; ## Table of Contents
 
@@ -41,47 +41,47 @@
 
 ;; ## Installation
 
-;; Install [pulsectl from MELPA](http://melpa.org/#/pulsectl), or put `pulsectl.el` in your load-path and do a `(require 'pulsectl)'.
+;; Install [pulseaudio-control from MELPA](http://melpa.org/#/pulseaudio-control), or put `pulseaudio-control.el` in your load-path and do a `(require 'pulseaudio-control)'.
 
 ;; ## Usage
 
-;; Use C-x / to access the `pulsectl' keymap.  The default keybindings in that keymap are:
+;; Use C-x / to access the `pulseaudio-control' keymap.  The default keybindings in that keymap are:
 
-;; * + : Increase the volume of the currently-selected sink by `pulsectl-volume-step' (`pulsectl-increase-volume').
+;; * + : Increase the volume of the currently-selected sink by `pulseaudio-control-volume-step' (`pulseaudio-control-increase-volume').
 
-;; * - : Decrease the volume of the currently-selected sink by `pulsectl-volume-step' (`pulsectl-decrease-volume').
+;; * - : Decrease the volume of the currently-selected sink by `pulseaudio-control-volume-step' (`pulseaudio-control-decrease-volume').
 
-;; * v : Directly specify the volume of the currently-selected sink (`pulsectl-set-volume').  The value can be:
+;; * v : Directly specify the volume of the currently-selected sink (`pulseaudio-control-set-volume').  The value can be:
 
 ;;   * a percentage, e.g. '10%';
 ;;   * in decibels, e.g. '2dB';
 ;;   * a linear factor, e.g. '0.9' or '1.1'.
 
-;; * m : Toggle muting of the currently-selected sink (`pulsectl-toggle-current-sink-mute').
+;; * m : Toggle muting of the currently-selected sink (`pulseaudio-control-toggle-current-sink-mute').
 
-;; * x : Toggle muting of a sink, specified by index (`pulsectl-toggle-sink-mute-by-index').
+;; * x : Toggle muting of a sink, specified by index (`pulseaudio-control-toggle-sink-mute-by-index').
 
-;; * e : Toggle muting of a sink, specified by name (`pulsectl-toggle-sink-mute-by-name').
+;; * e : Toggle muting of a sink, specified by name (`pulseaudio-control-toggle-sink-mute-by-name').
 
-;; * i : Select a sink to be the current sink, specified by index (`pulsectl-select-sink-by-index').
+;; * i : Select a sink to be the current sink, specified by index (`pulseaudio-control-select-sink-by-index').
 
-;; * n : Select a sink to be the current sink, specified by name (`pulsectl-select-sink-by-name').
+;; * n : Select a sink to be the current sink, specified by name (`pulseaudio-control-select-sink-by-name').
 
-;; Customisation options, including `pulsectl-volume-step', are available via the `pulsectl' customize-group.
+;; Customisation options, including `pulseaudio-control-volume-step', are available via the `pulseaudio-control' customize-group.
 
 ;; ## Issues / bugs
 
-;; If you discover an issue or bug in `pulsectl' not already noted:
+;; If you discover an issue or bug in `pulseaudio-control' not already noted:
 
 ;; * as a TODO item, or
 
-;; * in [the project's "Issues" section on GitHub](https://github.com/flexibeast/pulsectl/issues),
+;; * in [the project's "Issues" section on GitHub](https://github.com/flexibeast/pulseaudio-control/issues),
 
 ;; please create a new issue with as much detail as possible, including:
 
 ;; * which version of Emacs you're running on which operating system, and
 
-;; * how you installed `pulsectl'.
+;; * how you installed `pulseaudio-control'.
 
 ;; ## License
 
@@ -92,21 +92,21 @@
 
 ;; Customisable variables.
 
-(defgroup pulsectl nil
+(defgroup pulseaudio-control nil
   "Control PulseAudio volumes via `pactl'."
   :group 'external)
 
-(defcustom pulsectl-default-sink "0"
+(defcustom pulseaudio-control-default-sink "0"
   "Default Pulse sink index to act on."
   :type 'string
-  :group 'pulsectl)
+  :group 'pulseaudio-control)
 
-(defcustom pulsectl-pactl-path "/usr/bin/pactl"
+(defcustom pulseaudio-control-pactl-path "/usr/bin/pactl"
   "Absolute path of `pactl' executable."
   :type '(file :must-match t)
-  :group 'pulsectl)
+  :group 'pulseaudio-control)
 
-(defcustom pulsectl-volume-step "10%"
+(defcustom pulseaudio-control-volume-step "10%"
   "Step to use when increasing or decreasing volume.
 
 The value can be:
@@ -115,34 +115,34 @@ The value can be:
 * in decibels, e.g. '2dB';
 * a linear factor, e.g. '0.9' or '1.1'."
   :type 'string
-  :group 'pulsectl)
+  :group 'pulseaudio-control)
 
 
 ;; Internal variables.
 
-(defvar pulsectl--current-sink pulsectl-default-sink
+(defvar pulseaudio-control--current-sink pulseaudio-control-default-sink
   "String containing index of currently-selected Pulse sink.")
 
 
 ;; Internal functions.
 
-(defun pulsectl--call-pactl (command)
+(defun pulseaudio-control--call-pactl (command)
   "Call `pactl' with COMMAND as its arguments.
 
 COMMAND is a single string separated by spaces,
 e.g. 'list short sinks'."
   (let ((args `("" nil
-                ,pulsectl-pactl-path
+                ,pulseaudio-control-pactl-path
                 nil t nil
                 ,@(split-string command " "))))
     (apply #'call-process-region args)))
 
-(defun pulsectl--get-sinks ()
+(defun pulseaudio-control--get-sinks ()
   "Internal function; get a list of Pulse sinks via `pactl'."
   (let ((fields-re "^\\(\\S-+\\)\\s-+\\(\\S-+\\)")
         (sinks '()))
     (with-temp-buffer
-      (pulsectl--call-pactl "list short sinks")
+      (pulseaudio-control--call-pactl "list short sinks")
       (goto-char (point-min))
       (while (re-search-forward fields-re nil t)
         (let ((number (match-string 1))
@@ -154,29 +154,29 @@ e.g. 'list short sinks'."
 ;; User-facing functions.
 
 ;;;###autoload
-(defun pulsectl-decrease-volume ()
+(defun pulseaudio-control-decrease-volume ()
   "Decrease volume of currently-selected Pulse sink.
 
-Amount of decrease is specified by `pulsectl-volume-step'."
+Amount of decrease is specified by `pulseaudio-control-volume-step'."
   (interactive)
-  (pulsectl--call-pactl (concat "set-sink-volume "
-                                pulsectl--current-sink
-                                " -"
-                                pulsectl-volume-step)))
+  (pulseaudio-control--call-pactl (concat "set-sink-volume "
+                                          pulseaudio-control--current-sink
+                                          " -"
+                                          pulseaudio-control-volume-step)))
 
 ;;;###autoload
-(defun pulsectl-increase-volume ()
+(defun pulseaudio-control-increase-volume ()
   "Increase volume of currently-selected Pulse sink.
 
-Amount of increase is specified by `pulsectl-volume-step'."
+Amount of increase is specified by `pulseaudio-control-volume-step'."
   (interactive)
-  (pulsectl--call-pactl (concat "set-sink-volume "
-                                pulsectl--current-sink
-                                " +"
-                                pulsectl-volume-step)))
+  (pulseaudio-control--call-pactl (concat "set-sink-volume "
+                                          pulseaudio-control--current-sink
+                                          " +"
+                                          pulseaudio-control-volume-step)))
 
 ;;;###autoload
-(defun pulsectl-select-sink-by-index (sink)
+(defun pulseaudio-control-select-sink-by-index (sink)
   "Select which Pulse sink to act on, by numeric index.
 
 Accepts number as prefix argument.
@@ -184,7 +184,7 @@ Accepts number as prefix argument.
 Argument SINK is the number provided by the user."
   (interactive "NSink index: ")
   (let ((sink (number-to-string sink))
-        (valid-sinks (mapcar 'car (pulsectl--get-sinks))))
+        (valid-sinks (mapcar 'car (pulseaudio-control--get-sinks))))
     (if (member sink valid-sinks)
         (progn
           ;;
@@ -200,26 +200,26 @@ Argument SINK is the number provided by the user."
           ;; However, as at 20170828, it seems to work with
           ;; a numeric index also.
           ;;
-          (pulsectl--call-pactl (concat "set-default-sink "
-                                        sink))
-          (setq pulsectl--current-sink sink))
+          (pulseaudio-control--call-pactl (concat "set-default-sink "
+                                                  sink))
+          (setq pulseaudio-control--current-sink sink))
       (error "Invalid sink index"))))
 
 ;;;###autoload
-(defun pulsectl-select-sink-by-name ()
+(defun pulseaudio-control-select-sink-by-name ()
   "Select which Pulse sink to act on, by name."
   (interactive)
-  (let* ((valid-sinks (mapcar 'cdr (pulsectl--get-sinks)))
+  (let* ((valid-sinks (mapcar 'cdr (pulseaudio-control--get-sinks)))
          (sink (completing-read "Sink name: " valid-sinks))) 
     (if (member sink valid-sinks)
         (progn
-          (pulsectl--call-pactl (concat "set-default-sink "
-                                        sink))
-          (setq pulsectl--current-sink sink))
+          (pulseaudio-control--call-pactl (concat "set-default-sink "
+                                                  sink))
+          (setq pulseaudio-control--current-sink sink))
       (error "Invalid sink name"))))
 
 ;;;###autoload
-(defun pulsectl-set-volume (volume)
+(defun pulseaudio-control-set-volume (volume)
   "Set volume of currently-selected Pulse sink.
 
 The value can be:
@@ -235,65 +235,65 @@ Argument VOLUME is the volume provided by the user."
                            "\\|[[:digit:]]+dB"
                            "\\|[[:digit:]]+\.[[:digit:]]+")))
     (if (string-match valid-volumes-re volume)
-        (pulsectl--call-pactl (concat "set-sink-volume "
-                                      pulsectl--current-sink
-                                      " "
-                                      volume))
+        (pulseaudio-control--call-pactl (concat "set-sink-volume "
+                                                pulseaudio-control--current-sink
+                                                " "
+                                                volume))
       (error "Invalid volume"))))
 
 ;;;###autoload
-(defun pulsectl-toggle-current-sink-mute ()
+(defun pulseaudio-control-toggle-current-sink-mute ()
   "Toggle muting of currently-selected Pulse sink."
   (interactive)
-  (pulsectl--call-pactl (concat "set-sink-mute "
-                                pulsectl--current-sink
-                                " toggle")))
+  (pulseaudio-control--call-pactl (concat "set-sink-mute "
+                                          pulseaudio-control--current-sink
+                                          " toggle")))
 
 ;;;###autoload
-(defun pulsectl-toggle-sink-mute-by-index (sink)
+(defun pulseaudio-control-toggle-sink-mute-by-index (sink)
   "Toggle muting of Pulse sink, specified by index.
 
 Argument SINK is the number provided by the user."
   (interactive "NSink index: ")
   (let ((sink (number-to-string sink))
-        (valid-sinks (mapcar 'car (pulsectl--get-sinks))))
+        (valid-sinks (mapcar 'car (pulseaudio-control--get-sinks))))
     (if (member sink valid-sinks)
         (progn
-          (pulsectl--call-pactl (concat "set-sink-mute "
-                                        sink
-                                        " toggle")))
+          (pulseaudio-control--call-pactl (concat "set-sink-mute "
+                                                  sink
+                                                  " toggle")))
       (error "Invalid sink index"))))
 
 ;;;###autoload
-(defun pulsectl-toggle-sink-mute-by-name ()
+(defun pulseaudio-control-toggle-sink-mute-by-name ()
   "Toggle muting of Pulse sink, specified by name."
   (interactive)
-  (let* ((valid-sinks (mapcar 'cdr (pulsectl--get-sinks)))
+  (let* ((valid-sinks (mapcar 'cdr (pulseaudio-control--get-sinks)))
          (sink (completing-read "Sink name: " valid-sinks))) 
     (if (member sink valid-sinks)
         (progn
-          (pulsectl--call-pactl (concat "set-sink-mute "
-                                        sink
-                                        " toggle")))
+          (pulseaudio-control--call-pactl (concat "set-sink-mute "
+                                                  sink
+                                                  " toggle")))
       (error "Invalid sink name"))))
 
 
 ;; Default keymap.
 
-(define-prefix-command 'pulsectl-map)
-(global-set-key (kbd "C-x /") 'pulsectl-map)
-(define-key pulsectl-map (kbd "-") 'pulsectl-decrease-volume)
-(define-key pulsectl-map (kbd "+") 'pulsectl-increase-volume)
-(define-key pulsectl-map (kbd "m") 'pulsectl-toggle-current-sink-mute)
-(define-key pulsectl-map (kbd "x") 'pulsectl-toggle-sink-mute-by-index)
-(define-key pulsectl-map (kbd "e") 'pulsectl-toggle-sink-mute-by-name)
-(define-key pulsectl-map (kbd "i") 'pulsectl-select-sink-by-index)
-(define-key pulsectl-map (kbd "n") 'pulsectl-select-sink-by-name)
-(define-key pulsectl-map (kbd "v") 'pulsectl-set-volume)
+(define-prefix-command 'pulseaudio-control-map)
+(global-set-key (kbd "C-x /") 'pulseaudio-control-map)
+(define-key pulseaudio-control-map (kbd "-") 'pulseaudio-control-decrease-volume)
+(define-key pulseaudio-control-map (kbd "+") 'pulseaudio-control-increase-volume)
+(define-key pulseaudio-control-map (kbd "m") 'pulseaudio-control-toggle-current-sink-mute)
+(define-key pulseaudio-control-map (kbd "x") 'pulseaudio-control-toggle-sink-mute-by-index)
+(define-key pulseaudio-control-map (kbd "e") 'pulseaudio-control-toggle-sink-mute-by-name)
+(define-key pulseaudio-control-map (kbd "i") 'pulseaudio-control-select-sink-by-index)
+(define-key pulseaudio-control-map (kbd "n") 'pulseaudio-control-select-sink-by-name)
+(define-key pulseaudio-control-map (kbd "v") 'pulseaudio-control-set-volume)
 
 
 ;; --
 
-(provide 'pulsectl)
+(provide 'pulseaudio-control)
 
-;;; pulsectl.el ends here
+;;; pulseaudio-control.el ends here
